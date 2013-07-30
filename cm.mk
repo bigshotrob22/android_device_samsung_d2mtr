883a829a793a66c245b0e57d576b7dae8ac07aec
Dan Pasanen <dan.pasanen@gmail.com>
2013-07-30 12:33:51 -0500

    update fingerprint
    Change-Id: I5196cf2c1e54d353bed02daa5f27106420003f0d

--- cm.mk
+++ cm.mk
@@ -9,7 +9,11 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2mtr TARGET_DEVICE=d2mtr BUILD_FINGERPRINT="samsung/d2mtr/d2mtr:4.0.4/IMM76D/R530MVQALJ1:user/release-keys" PRIVATE_BUILD_DESC="d2mtr-user 4.0.4 IMM76D R530MVQALJ1 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=d2mtr \
+    TARGET_DEVICE=d2mtr \
+    BUILD_FINGERPRINT="samsung/d2mtr/d2mtr:4.1.2/JZO54K/R530MVQAMF2:user/release-keys" \
+    PRIVATE_BUILD_DESC="d2mtr-user 4.1.2 JZO54K R530MVQAMF2 release-keys"
 
 PRODUCT_NAME := cm_d2mtr
 PRODUCT_DEVICE := d2mtr
